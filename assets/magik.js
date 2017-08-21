9f575192df4ada0a2225e17ea1d7b0e1cc59f76b
Austin Melms <memyselfandlie@gmail.com>
2017-08-20 23:07:09 -0400

    s

--- assets/magik.js
+++ assets/magik.js
@@ -11,7 +11,7 @@ exports.run = (dataURL) => {
 		if (data.status !== 200)
 			return reject(data.status)
 		gm(data.body)
-			.out('-liquid-rescale', '300%', '-liquid-rescale', '100%')
+			.out('-liquid-rescale', '25%', '-liquid-rescale', '600%')
 			//.implode(-1)
 			//.swirl(`${getRandomInt(0, 1) === 1 ? '+' : '-'}${getRandomInt(40, 80)}`)
 			.toBuffer('PNG', (err, buffer) => {
