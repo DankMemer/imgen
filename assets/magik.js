db4666fb692371d18c1d40929dbff061267fc01f
Austin Melms <memyselfandlie@gmail.com>
2017-08-20 23:10:45 -0400

    bvcx

--- assets/magik.js
+++ assets/magik.js
@@ -11,7 +11,7 @@ exports.run = (dataURL) => {
 		if (data.status !== 200)
 			return reject(data.status)
 		gm(data.body)
-			.out('-liquid-rescale', '25%', '-liquid-rescale', '600%')
+			.out('-liquid-rescale', '60%', '-liquid-rescale', '600%')
 			//.implode(-1)
 			//.swirl(`${getRandomInt(0, 1) === 1 ? '+' : '-'}${getRandomInt(40, 80)}`)
 			.toBuffer('PNG', (err, buffer) => {
