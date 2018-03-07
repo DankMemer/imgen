2707b75e4f95af2e48305985219cbb23e165f883
Luke <dev@crimsonxv.pro>
2018-03-07 05:36:08 +0000

    ???

--- assets/warp.js
+++ assets/warp.js
@@ -5,7 +5,7 @@ const gm = require('gm').subClass({
 
 exports.run = (dataURL) => {
   return new Promise(async (resolve, reject) => {
-    let data = await sf.get(dataURL)
+    let data = await sf.get(dataURL).catch(err => reject(err))
     if (data.status !== 200) { return reject(new Error('Server returned ' + data.status)) }
     gm(data.body).implode(`-${getRandomInt(3, 15)}`).roll(`+${getRandomInt(0, 256)}+${getRandomInt(0, 256)}`).swirl(`${getRandomInt(0, 1) === 1 ? '+' : '-'}${getRandomInt(120, 180)}`).toBuffer('PNG', (err, buf) => {
       if (err) { return reject(err) }
