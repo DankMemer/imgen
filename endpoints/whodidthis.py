adf7a6e6c8caef92213042f56e85b032d0940f26
Devoxin <lukel9914@gmail.com>
2018-05-18 02:04:15 +0100

    who did this endpoint

---
+++ endpoints/whodidthis.py
@@ -0,0 +1,23 @@
+from io import BytesIO
+
+from flask import send_file
+from PIL import Image
+
+from utils import http
+from utils.endpoint import Endpoint
+
+
+class Whodidthis(Endpoint):
+    def generate(self, avatars, text, usernames):
+        base = Image.open('assets/whodidthis/whodidthis.png')
+        avatar = Image.open(http.get_image(avatars[0])).resize((720, 405)).convert('RGBA')
+        base.paste(avatar, (0, 159), avatar)
+
+        b = BytesIO()
+        base.save(b, format='png')
+        b.seek(0)
+        return send_file(b, mimetype='image/png')
+
+
+def setup():
+    return Whodidthis()
