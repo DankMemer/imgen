b066acaf55b6220e09b49e9faceae810351ab1e2
Devoxin <lukel9914@gmail.com>
2018-05-20 01:43:00 +0100

    this is actually a negative

--- endpoints/warp.py
+++ endpoints/warp.py
@@ -9,7 +9,7 @@ from utils.endpoint import Endpoint
 
 class Warp(Endpoint):
     def generate(self, avatars, text, usernames):
-        implode = str(randint(3, 15))
+        implode = f'-{str(randint(3, 15))}'
         roll = f'+{randint(0, 256)}+{randint(0, 256)}'
         swirl = f'{choice(["+", "-"])}{randint(120, 180)}'
         concat = ['-implode', implode, '-roll', roll, '-swirl', swirl]
