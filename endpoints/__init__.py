31579e9ec0829aaaeebaa13625fac09e6e803a4c
Devoxin <lukel9914@gmail.com>
2018-06-13 19:25:33 +0100

    Add back magik

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -3,7 +3,7 @@ import sys
 from .import (abandon, ban, bed, brain,  # noqa: F401
               byemom, disability, facts, gay, hitler, invert, jail,
               quote, shit, slap, spank, trash, trigger, tweet, ugly,
-              warp, whodidthis)
+              warp, whodidthis, magik)
 
 endpoints = {}
 
---
+++ endpoints/magik.py
@@ -0,0 +1,31 @@
+from io import BytesIO
+
+from flask import send_file
+from wand import image
+
+from utils import http
+from utils.endpoint import Endpoint
+
+
+class Magik(Endpoint):
+    def generate(self, avatars, text, usernames):
+        avatar = http.get_image(avatars[0])
+        with image.Image(file=avatar) as img:
+            img.transform(resize='400x400')
+            img.liquid_rescale(width=int(img.width * 0.5),
+                               height=int(img.height * 0.5),
+                               delta_x=0.5,
+                               rigidity=0)
+            img.liquid_rescale(width=int(img.width * 1.5),
+                               height=int(img.height * 1.5),
+                               delta_x=2,
+                               rigidity=0)
+
+            b = BytesIO()
+            img.save(file=b)
+            b.seek(0)
+            return send_file(b, mimetype='image/png')
+
+
+def setup():
+    return Magik()
