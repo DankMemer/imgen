d931e259ad27619519ebd8333452e21ef4bbfa03
Austin Melms <memyselfandlie@gmail.com>
2018-06-16 22:00:52 -0400

    ban => sickfilth
    moved this endpoint to a new name to make room for the real ban command

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,8 +1,8 @@
 import sys
 
-from .import (abandon, ban, bed, brain,  # noqa: F401
+from .import (abandon, bed, brain,  # noqa: F401
               byemom, disability, facts, gay, hitler, invert, jail,
-              quote, shit, slap, spank, trash, trigger, tweet, ugly,
+              quote, shit, sickfilth, slap, spank, trash, trigger, tweet, ugly,
               warp, whodidthis, magik, deepfry)
 
 endpoints = {}
