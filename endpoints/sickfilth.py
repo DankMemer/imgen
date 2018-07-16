ffc20e9e2c834ef799ee36261be6ffb2317a90d8
Perry Fraser <perry@frasers.org>
2018-07-15 23:18:52 -0400

    Make SickFilth match bot (SickFilth -> SickBan)

--- endpoints/sickfilth.py
+++ endpoints/sickfilth.py
@@ -7,7 +7,7 @@ from utils import http
 from utils.endpoint import Endpoint
 
 
-class SickFilth(Endpoint):
+class SickBan(Endpoint):
     def generate(self, avatars, text, usernames):
         base = Image.open('assets/ban/ban.png').convert('RGBA')
         avatar = Image.open(http.get_image(avatars[0])).resize((400, 400)).convert('RGBA')
@@ -20,4 +20,4 @@ class SickFilth(Endpoint):
 
 
 def setup():
-    return SickFilth()
+    return SickBan()
