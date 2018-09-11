eacc68cfc1ec954485eb59288ddfe16bedce8d6c
Perry Fraser <perryprog@users.noreply.github.com>
2018-09-11 18:12:17 -0400

    Fix stupid bug

--- endpoints/wanted.py
+++ endpoints/wanted.py
@@ -9,7 +9,7 @@ from utils.endpoint import Endpoint
 
 class Wanted(Endpoint):
     def generate(self, avatars, text, usernames):
-        base = Image.open('assets/Wanted/wanted.png').convert('RGBA')
+        base = Image.open('assets/wanted/wanted.png').convert('RGBA')
         avatar = Image.open(http.get_image(avatars[0])).resize((447, 447)).convert('RGBA')
         base.paste(avatar, (145, 282), avatar)
 
