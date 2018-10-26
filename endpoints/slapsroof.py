1800943e9299d58df4caf2ad8b0588e784d89e75
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2018-10-26 22:29:41 +0200

    Added youtube, slapsroof and bongocat endpoints

---
+++ endpoints/slapsroof.py
@@ -0,0 +1,26 @@
+from io import BytesIO
+
+from flask import send_file
+from PIL import Image, ImageDraw, ImageFont
+
+from utils.endpoint import Endpoint
+from utils.textutils import wrap
+
+
+class SlapsRoof(Endpoint):
+    def generate(self, avatars, text, usernames):
+        base = Image.open('assets/slapsroof/slapsroof.png')
+        font = ImageFont.truetype(font='assets/fonts/medium.woff', size=33, )
+        canv = ImageDraw.Draw(base)
+        suffix = ' in it'
+        text = wrap(font, text + suffix, 1150)
+        canv.text((335, 31), text, font=font, fill='Black')
+
+        b = BytesIO()
+        base.save(b, format='png')
+        b.seek(0)
+        return send_file(b, mimetype='image/png')
+
+
+def setup():
+    return SlapsRoof()
---
+++ endpoints/youtube.py
@@ -0,0 +1,46 @@
+from io import BytesIO
+
+from flask import send_file
+from PIL import Image, ImageDraw, ImageFont
+
+from utils import http
+from utils.endpoint import Endpoint
+from utils.textutils import wrap
+
+from random import randint
+
+
+class Youtube(Endpoint):
+    def generate(self, avatars, text, usernames):
+        avatar = http.get_image(avatars[0]).resize((52, 52)).convert('RGBA')
+        name = usernames[0]
+        base = Image.open('assets/youtube/youtube.png').convert('RGBA')
+        font = ImageFont.truetype(font='assets/fonts/robotomedium.ttf', size=17, )
+        font2 = ImageFont.truetype(font='assets/fonts/robotoregular.ttf', size=17, )
+        font3 = ImageFont.truetype(font='assets/fonts/robotoregular.ttf', size=19, )
+
+        bigsize = (avatar.size[0] * 3, avatar.size[1] * 3)
+        mask = Image.new('L', bigsize, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + bigsize, fill=255)
+        mask = mask.resize(avatar.size, Image.ANTIALIAS)
+        avatar.putalpha(mask)
+
+        base.paste(avatar, (17, 33), avatar)
+        canv = ImageDraw.Draw(base)
+        op = wrap(font, name, 1150)
+        size = canv.textsize(name, font=font)
+        comment = wrap(font3, text, 550)
+        time = f'{randint(1,60)} minutes ago'
+        canv.text((92, 34), op, font=font, fill='Black')
+        canv.text((100 + size[0], 34), time, font=font2, fill='Grey')
+        canv.text((92, 59), comment, font=font3, fill='Black')
+
+        b = BytesIO()
+        base.save(b, format='png')
+        b.seek(0)
+        return send_file(b, mimetype='image/png')
+
+
+def setup():
+    return Youtube()
