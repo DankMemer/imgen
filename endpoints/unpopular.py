c654b6dd1e77f6c3df2413d9d2311d7a84e2a710
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2018-11-01 14:20:18 +0100

    Add unpopular endpoint

---
+++ endpoints/unpopular.py
@@ -0,0 +1,48 @@
+from io import BytesIO
+
+from flask import send_file
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+
+from utils import http
+from utils.endpoint import Endpoint
+from utils.textutils import wrap
+
+
+class Unpopular(Endpoint):
+    def generate(self, avatars, text, usernames):
+        avatar = http.get_image(avatars[0]).resize((666, 666)).convert('RGBA')
+        base = Image.open('assets/unpopular/unpopular.png').convert('RGBA')
+        font = ImageFont.truetype(font='assets/fonts/semibold.woff', size=100, )
+        reticle = Image.open('assets/unpopular/reticle.png').convert('RGBA')
+        temp = Image.new('RGBA', (1200, 800), color=(0, 0, 0, 0))
+        avatar_square = Image.new(mode='RGBA', size=(340, 260), color=(0, 0, 0, 0))
+        avatar_mono = avatar.resize((300, 300)).rotate(16, expand=1).convert('1')
+        avatar_darkened = ImageEnhance.Brightness(avatar_mono.convert('RGB')).enhance(0.5)
+        avatar_square.paste(avatar_darkened, (0, 0), avatar_mono)
+
+        bigsize = (avatar.size[0] * 3, avatar.size[1] * 3)
+        mask = Image.new('L', bigsize, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + bigsize, fill=255)
+        mask = mask.resize(avatar.size, Image.ANTIALIAS)
+        avatar.putalpha(mask)
+
+        base.paste(avatar, (1169, 1169), avatar)
+        face = avatar.resize((368, 368))
+        base.paste(face, (140,250), face)
+        base.paste(reticle, (1086, 1086), reticle)
+        base.paste(avatar_square, (-20, 1670), avatar_square)
+        canv = ImageDraw.Draw(temp)
+        wrapped = wrap(font, text, 1150)
+        canv.text((0, 0), wrapped, font=font, fill='Black')
+        w = temp.rotate(1, expand=1)
+        base.paste(w, (620, 280), w)
+
+        b = BytesIO()
+        base.save(b, format='png')
+        b.seek(0)
+        return send_file(b, mimetype='image/png')
+
+
+def setup():
+    return Unpopular()
