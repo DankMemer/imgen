309529f59103650c6757836d6534392e271f89c4
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2018-11-20 00:26:12 +0100

    Actually add new pls tweet

--- endpoints/tweet.py
+++ endpoints/tweet.py
@@ -3,18 +3,52 @@ from io import BytesIO
 from PIL import Image, ImageDraw
 from flask import send_file
 
+from utils import http
 from utils.endpoint import Endpoint
 from utils.textutils import wrap
+
+from datetime import datetime
+from random import randint
 
 
 class Tweet(Endpoint):
     def generate(self, avatars, text, usernames):
         base = Image.open(self.assets.get('assets/tweet/trump.bmp'))
-        font = self.assets.get_font('assets/fonts/segoeuireg.ttf', size=50)
+        avatar = http.get_image(avatars[0]).resize((98, 98)).convert('RGBA')
+        font = self.assets.get_font('assets/fonts/segoeuireg.ttf', size=50, )
+        font2 = self.assets.get_font('assets/fonts/robotomedium.ttf', size=40)
+        font3 = self.assets.get_font('assets/fonts/robotoregular.ttf', size=29)
+        font4 = self.assets.get_font('assets/fonts/robotoregular.ttf', size=35)
+
+        circle = Image.new('L', (20, 20), 0)
+        draw = ImageDraw.Draw(circle)
+        draw.ellipse((0, 0, 20, 20), fill=255)
+        alpha = Image.new('L', avatar.size, 255)
+        w, h = avatar.size
+        alpha.paste(circle.crop((0, 0, 10, 10)), (0, 0))
+        alpha.paste(circle.crop((0, 10, 10, 10 * 2)), (0, h - 10))
+        alpha.paste(circle.crop((10, 0, 10 * 2, 10)), (w - 10, 0))
+        alpha.paste(circle.crop((10, 10, 10 * 2, 10 * 2)), (w - 10, h - 10))
+        avatar.putalpha(alpha)
+
+        base.paste(avatar, (42, 38), avatar)
         canv = ImageDraw.Draw(base)
         text = wrap(font, text, 1150)
+        text2 = wrap(font2, usernames[0], 1150)
+        text3 = wrap(font3, f'@{usernames[0]}', 1150)
+
+        time = datetime.now().strftime('%-I:%M %p - %d %b %Y')
+        retweets = "{:,}".format(randint(0, 99999))
+        likes = "{:,}".format(randint(0, 99999))
+        text4 = wrap(font3, time, 1150)
+        text5 = wrap(font4, retweets, 1150)
+        text6 = wrap(font4, likes, 1150)
         canv.text((45, 160), text, font=font, fill='Black')
-        base = base.convert('RGB')
+        canv.text((160, 45), text2, font=font2, fill='Black')
+        canv.text((160, 95), text3, font=font3, fill='Grey')
+        canv.text((40, 570), text4, font=font3, fill='Grey')
+        canv.text((40, 486), text5, font=font4, fill='#2C5F63')
+        canv.text((205, 486), text6, font=font4, fill='#2C5F63')
 
         b = BytesIO()
         base.save(b, format='jpeg')
