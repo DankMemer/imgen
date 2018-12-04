8eea2d25aa400624dc6b2d71555921f650d3840c
AndyTempel <andraz@korenc.eu>
2018-12-04 20:08:27 +0100

    docs fixes

--- utils/db.py
+++ utils/db.py
@@ -1,6 +1,5 @@
 import json
 
-import redis
 import rethinkdb as r
 from flask import g
 
@@ -15,9 +14,3 @@ def get_db():
     if 'rdb' not in g:
         g.rdb = r.connect(RDB_ADDRESS, RDB_PORT, db=RDB_DB)
     return g.rdb
-
-
-def get_redis():
-    if 'redis' not in g:
-        g.redis = redis.Redis(db=9)
-    return g.redis
