554b269a6ebc8752d8280e28bd56c24f35eb50a4
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2018-12-04 15:42:35 +0100

    - Fixed breaking comma stuffs whahahoops

--- endpoints/surprised.py
+++ endpoints/surprised.py
@@ -15,9 +15,9 @@ class Surprised(Endpoint):
         base = Image.open(self.assets.get('assets/surprised/surprised.bmp')).convert('RGBA')
         font = self.assets.get_font('assets/fonts/robotoregular.ttf', size=36)
         try:
-            text1, text2 = text.split(', ')
+            text1, text2 = text.split.replace(', ', ',').split(',')
         except ValueError:
-            text1, text2 = 'tries to use surprised without splitting by comma, the command breaks'.split(', ')
+            text1, text2 = 'tries to use surprised without splitting by comma,the command breaks'.split(',')
         text1 = wrap(font, 'me: ' + text1, 650)
         text2 = wrap(font, 'also me: ' + text2, 650)
         canv = ImageDraw.Draw(base)
