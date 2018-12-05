182b8e507bcf4779b69982939f7844948309e584
Devoxin <lukel9914@gmail.com>
2018-12-05 21:14:34 +0000

    Prevent large images crushing the server

--- utils/http.py
+++ utils/http.py
@@ -5,22 +5,31 @@ import requests
 from PIL import Image
 
 config = json.load(open('config.json'))
+MAX_FILE_SIZE = config.get('max_file_size', 5000000)  # in bytes
 
 
-def get_image(url):
+def get(url, **kwargs):
+    if 'proxy_url' in config:
+        res = requests.get(config['proxy_url'], params={'url': url}, **kwargs)
+    else:
+        res = requests.get(url, **kwargs)
+
+    if 'content-length' not in res.headers:
+        raise KeyError(f'{url} is missing `content-length` header')
+
+    if int(res.headers.get('content-length', 0)) > MAX_FILE_SIZE:
+        raise OverflowError(f'content-length may not exceed {MAX_FILE_SIZE}')
+
+    return res
+
+
+def get_content_raw(url, **kwargs):
+    return get(url, stream=True, **kwargs).content
+
+
+def get_image(url, **kwargs):
     try:
-        if 'proxy_url' in config:
-            return Image.open(BytesIO(requests.get(config['proxy_url'],
-                                                   params={'url': url}, stream=True).content))
-        else:
-            return Image.open(BytesIO(requests.get(url, stream=True).content))
+        raw = get_content_raw(url, **kwargs)
+        return Image.open(BytesIO(raw))
     except OSError:
         raise TypeError('An invalid image was provided! Check the URL and try again.')
-
-
-def get_image_raw(url):
-    if 'proxy_url' in config:
-        return requests.get(config['proxy_url'],
-                            params={'url': url}, stream=True).content
-    else:
-        return requests.get(url, stream=True).content
