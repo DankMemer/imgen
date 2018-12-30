926d7e75b9307464725b864b5c2c61aaa6e154bc
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2018-12-30 21:49:51 +0100

    Implemented suggestions by @Devoxin, little fade in on the text and other small fixes 🦀 DEVOXINS WHINING IS GONE 🦀

--- endpoints/crab.py
+++ endpoints/crab.py
@@ -9,7 +9,7 @@ from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
 @setup
 class Crab(Endpoint):
-    """his endpoint returns an MP4 file. Make sure your application knows how to handle this format."""
+    """This endpoint returns an MP4 file. Make sure your application knows how to handle this format."""
     params = ['text']
 
     def generate(self, avatars, text, usernames, kwargs):
@@ -21,16 +21,16 @@ class Crab(Endpoint):
         t = text.upper().replace(', ', ',').split(',')
         clip = VideoFileClip("assets/crab/template.mp4")
 
-        text = TextClip(t[0], fontsize=48, color='white', font='Verdana')
+        text = TextClip(t[0] + '🅱️', fontsize=48, color='white', font='Verdana')
         text2 = TextClip("____________________", fontsize=48, color='white', font='Verdana')
-        text = text.set_position(("center", 200)).set_duration(15)
-        text2 = text2.set_position(("center", 210)).set_duration(15)
-        text3 = TextClip(t[1], fontsize=48, color='white', font='Verdana')
-        text3 = text3.set_position(("center", 270)).set_duration(15)
+        text = text.set_position(("center", 200)).set_duration(15.4)
+        text2 = text2.set_position(("center", 210)).set_duration(15.4)
+        text3 = TextClip(t[1] + '🅱️', fontsize=48, color='white', font='Verdana')
+        text3 = text3.set_position(("center", 270)).set_duration(15.4)
 
-        video = CompositeVideoClip([clip, text, text2, text3]).set_duration(15)
+        video = CompositeVideoClip([clip, text.crossfadein(1), text2.crossfadein(1), text3.crossfadein(1)]).set_duration(15.4)
 
-        name = str(uuid.uuid4().hex) + '.mp4'
+        name = uuid.uuid4().hex + '.mp4'
         video.write_videofile(name, threads=4, preset='superfast')
 
         return send_file(name, mimetype='video/mp4')
