e5868f5376838b1acd77aa88a10da1a3ca0d08d4
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2019-01-03 13:53:08 +0100

    - Added the endpoints citation, presentation, stroke and sword - Added the font bmmini for citation

---
+++ endpoints/stroke.py
@@ -0,0 +1,25 @@
+from io import BytesIO
+
+from PIL import Image, ImageDraw
+from flask import send_file
+
+from utils.endpoint import Endpoint, setup
+from utils.textutils import wrap
+
+
+@setup
+class Stroke(Endpoint):
+    params = ['text']
+
+    def generate(self, avatars, text, usernames, kwargs):
+        base = Image.open(self.assets.get('assets/stroke/stroke.bmp'))
+        font = self.assets.get_font('assets/fonts/verdana.ttf', size=12)
+        canv = ImageDraw.Draw(base)
+        text = wrap(font, text, 75)
+        canv.text((272, 287), text, font=font, fill='Black')
+
+        base = base.convert('RGB')
+        b = BytesIO()
+        base.save(b, format='jpeg')
+        b.seek(0)
+        return send_file(b, mimetype='image/jpeg')
