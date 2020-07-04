425d75c876617bd163bb9e539ace17ea62ae598e
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2020-07-04 17:37:40 +0200

    Make yeeter.py load config data

--- yeeter.py
+++ yeeter.py
@@ -1,8 +1,16 @@
 import os
 import redis
 from time import time, sleep
+import json
 
-r = redis.Redis(db=1, decode_responses=True)
+config = json.load(open('config.json'))
+
+REDIS_PASSWORD = config.get('redis_password', '')
+REDIS_ADDRESS = config.get('redis_address', 'localhost')
+REDIS_PORT = config.get('redis_port', 6379)
+REDIS_DB = config.get('redis_db', 1)
+
+r = redis.Redis(password=REDIS_PASSWORD, host=REDIS_ADDRESS, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
 
 while 1:
     for file in os.listdir('cache'):
