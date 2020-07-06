5053ab5cbe3b96b14d389b40cde34080e5115c71
Geoffrey Westhoff <geoffreywesthoff@gmail.com>
2020-07-06 17:44:06 +0200

    Forgot to add the avatars as well

--- endpoints/obama.py
+++ endpoints/obama.py
@@ -5,6 +5,7 @@ from flask import send_file
 
 from utils.endpoint import Endpoint, setup
 from utils.textutils import render_text_with_emoji, wrap
+from utils import http
 
 
 @setup
@@ -16,7 +17,12 @@ class Obama(Endpoint):
         font = self.assets.get_font('assets/fonts/arimobold.ttf', size=36)
         canv = ImageDraw.Draw(base)
 
+        avatar = http.get_image(avatars[0]).resize((200, 200), Image.LANCZOS).convert('RGBA')
+
         w, _ = canv.textsize(wrap(font, usernames[0], 400), font)
+
+        base.paste(avatar, (120, 73), avatar)
+        base.paste(avatar, (365, 0), avatar)
 
         render_text_with_emoji(base, canv, (int(210 - (w/2)), 400), wrap(font, usernames[0], 400), font, 'white')
         render_text_with_emoji(base, canv, (int(470 - (w/2)), 300), wrap(font, usernames[0], 400), font, 'white')
